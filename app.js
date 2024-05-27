fb929dba79dfb6d9d436c35e9e7aba81f981244e
Subhransu Jena <subunc5@gmail.com>
2024-05-27 21:35:28 +0530

    Added timer functionality and polished it a bit.

--- app.js
+++ app.js
@@ -2,18 +2,56 @@
 const gameContainer = document.querySelector(".game-container");
 const inputSize = document.querySelectorAll(".input-size");
 const statusBox = document.querySelector(".status-box");
+const timerElement = document.querySelector(".timer");
+
 let gridSize = 5;
+let targetNumber = 1;
+let gameOver = false;
+let timer;
+let time = 0;
+
 inputSize.forEach((element) => {
   element.addEventListener("click", (e) => {
     targetNumber = 1;
     gameOver = false;
     gridSize = element.id;
+    stopTimer(timer);
+    resetTimer(timer);
     initializeGrid(element.id);
   });
 });
+gameContainer.addEventListener("click", () => {
+  gameContainer.style.filter = "none";
+  if (time == 0) startTimer();
+});
 
-let targetNumber = 1;
-let gameOver = false;
+function startTimer() {
+  clearInterval(timer);
+  timer = setInterval(() => {
+    time++;
+    updateTimer(timer);
+  }, 10);
+}
+
+function stopTimer() {
+  clearInterval(timer);
+}
+
+function resetTimer() {
+  clearInterval(timer);
+  time = 0;
+  updateTimer();
+}
+
+function updateTimer() {
+  const seconds = Math.floor(time / 100);
+  const milliSeconds = time - seconds * 100;
+  timerElement.textContent = `${padTimer(seconds)}:${padTimer(milliSeconds)}`;
+}
+
+function padTimer(number) {
+  return number.toString().padStart(2, "0");
+}
 
 // Generate the random number array
 
@@ -33,10 +71,14 @@ function initializeGrid(sizee) {
     }
     return arr;
   };
-  console.log(`${gridNumbers}`);
+  //console.log(`${gridNumbers}`);
   gridNumbers = fisherYates(gridNumbers);
-  console.log(`${gridNumbers}`);
+  //console.log(`${gridNumbers}`);
   statusUpdater(`${sizee} is the size now`);
+  setTimeout(() => {
+    if (statusBox.textContent.includes("is the size now"))
+      statusUpdater("Click to start the game!");
+  }, 2000);
   gameContainer.innerHTML = "";
   gameContainer.style.gridTemplateColumns = `repeat(${sizee}, 1fr)`;
 
@@ -47,29 +89,34 @@ function initializeGrid(sizee) {
     gameContainer.appendChild(cell);
     cell.addEventListener("click", handleCellClick);
   }
+  gameContainer.style.filter = "blur(60px)";
 }
 
 //update status
 function statusUpdater(message) {
+  if (message == "Wrong!") statusBox.style.color = "red";
+  else statusBox.style.color = "yellowgreen";
   statusBox.textContent = message;
 }
 
 // Handle cell click event
 function handleCellClick(event) {
+  if (gameContainer.style.filter != "none") return;
   const clickedNumber = parseInt(event.target.textContent);
 
   if (clickedNumber === targetNumber) {
     event.target.removeEventListener("click", handleCellClick);
     event.target.classList.add("right");
-    statusUpdater("right");
+    statusUpdater("Correct!!");
     targetNumber++;
 
     if (targetNumber > gridSize ** 2) {
       gameOver = true;
+      stopTimer();
       statusUpdater("YOU WON!!!");
     }
   } else {
-    statusUpdater("wrong");
+    statusUpdater("Wrong!");
     event.target.classList.add("wrong");
     setTimeout(() => event.target.classList.remove("wrong"), 1000);
   }
